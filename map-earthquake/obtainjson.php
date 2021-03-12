9b6cd79720b8f0e4fe189554932f10e1e1381616
JaimeMT <55758416+JaimeMT@users.noreply.github.com>
2021-03-12 17:47:09 +0100

    Update obtainjson.php

--- map-earthquake/obtainjson.php
+++ map-earthquake/obtainjson.php
@@ -1,35 +1,26 @@
 <?php
 
 header('Content-type: application/json');
-
 $terremotos = simplexml_load_file('http://www.ign.es/ign/RssTools/sismologia.xml');
-
 $datosTerremotos = $terremotos->channel->item; 
-
-$array = [];
-
-
+$quake = [];
 $geos = $terremotos->getNamespaces(true);
-
 foreach ($datosTerremotos as $datos) {
-
-    preg_match("/\d+\/\d+\/\d{4}/", $datos->title, $fecha);
-    preg_match("/\d+\:\d+\:\d+/", $datos->title, $hora);
-    preg_match("/[[:digit:]].\d/", $datos->description, $magnitud);
-    preg_match("/terremoto/", $datos->title, $titulo);
-
+    $patterns = ['/\d+\/\d+\/\d{4}/','/\d+\:\d+\:\d+/','/[[:digit:]].\d/','/terremoto/'];
+    preg_match($patterns[0], $datos->title, $fecha);
+    preg_match($patterns[1], $datos->title, $hora);
+    preg_match($patterns[2], $datos->description, $magnitud);
+    preg_match($patterns[3], $datos->title, $titulo);
     $geo = $datos->children($geos["geo"]);
-
-    array_push($array, [
-        'titulo' => (string)$titulo[0],
-        'link' => (string)$datos->link[0],
-        'fecha' => (string)$fecha[0],
-        'hora' => (string)$hora[0],
-        'magnitud' => (string)$magnitud[0],
+    array_push($quake, [
+        'titulo' => $titulo[0],
+        'link' => $datos->link[0],
+        'fecha' => $fecha[0],
+        'hora' => $hora[0],
+        'magnitud' => $magnitud[0],
         'lat' => (string)$geo->lat,
         'long' => (string)$geo->long
     ]);
 }
-
-echo json_encode($array);
+echo json_encode($quake);
 ?>
